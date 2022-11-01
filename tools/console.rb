fe4f618d0335e50d12d2d58b8aa5f459409c6fa8
Rajab <rkarume@gmail.com>
2022-11-01 03:09:02 +0300

    code challenge complete

--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,15 @@ end
 
 
 
+# author1 = Author.new("stephen ")
+
+# magazine1 = Magazine.new("men", "sports")
+
+# article1 = Article.new({"name": "stephen"}, {"name": "men", "category": "sport"}, "football")
+# article2 = Article.new("scott", "gatsby", "suspence")
+# article3 = Article.new("sydney", "tommorow", "fiction")
+# article4 = Article.new("charles", "moby", "whale")
+
 
 
 ### DO NOT REMOVE THIS
